953d065a564ec0623fcc61649746c89141417389
Karen-Victoria <kvictoria1000@gmail.com>
2025-07-11 19:33:21 -0300

    NLW Agents - 20
    Corrigindo a escrita para o código funcionar (esqueci de colocar o B maiúsculo kkkk)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiKeyInput = document.getElementById('apiKey')
 const gameSelect = document.getElementById('gameSelect')
 const questionInput = document.getElementById('questionInput')
-const askButton = document.getElementById('askbutton')
+const askButton = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
